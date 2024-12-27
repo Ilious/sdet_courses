05470cc8cba4b8297de121808284f1ab6b434810
I1nur <tuxtarov0475@gmail.com>
2024-12-27 18:03:11 +0400

    after merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 import lesson2.Login;
+import lesson2.WrongLoginException;
+import lesson2.WrongPasswordException;
 import lesson3.Money;
+import lesson_4.Lesson4;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-//        Lesson4 l4 = new Lesson4();
+        new Lesson4();
 //        printTasksTill4();
     }
 
